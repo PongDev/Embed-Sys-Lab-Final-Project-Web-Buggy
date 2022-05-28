78d74050c80b96883c30a049657dc953bd57df59
Pacharapon Arpanantikul <pacharaponarp@gmail.com>
2022-05-28 20:17:52 +0700

    implemented motor with variable speed

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -6,5 +6,8 @@ void motorA(char c,int speed);
 
 void motorB(char c,int speed);
 
-
+void forward();
+void backward();
+void left();
+void right();
 #endif
--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -1,50 +1,82 @@
+
+/*setup
+  HCLK 80 MHz
+  set Timer 1 and 3 ch 1,2 to PWM generation
+  prescaler = 8000 - 1
+  counter period = 100
+
+  put these in user code 2
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+
+ */
 #ifndef MOTOR_H_
 #define MOTOR_H_
 #include "main.h"
 
-#define motorB_pwn GPIO_PIN_1
-#define motorB_dir GPIO_PIN_0
-#define motorA_dir GPIO_PIN_8
-#define pwnB_ccr TIM1->CCR1
+#define pwnB_ccrA TIM1->CCR1
+#define pwnB_ccrB TIM1->CCR2
 #define pwnB_cnt TIM1->CNT
-#define pwnA_ccr TIM3->CCR3
-#define pwnA_cnt TIM3->CNT
 
+#define pwnA_ccrA TIM3->CCR1
+#define pwnA_ccrB TIM3->CCR2
+#define pwnA_cnt TIM3->CNT
 void motorA(char c,int speed){
 	switch(c){
 		case 'f':
-			HAL_GPIO_WritePin(GPIOC, motorA_dir, 1);
-			pwnA_ccr=speed;
+			pwnA_ccrA = speed;
+			pwnA_ccrB = 0;
 			pwnA_cnt=0;
 			break;
 		case 'b':
-			HAL_GPIO_WritePin(GPIOC, motorA_dir, 0);
-			pwnA_ccr=speed;
+			pwnA_ccrA = 0;
+			pwnA_ccrB = speed;
 			pwnA_cnt=0;
 			break;
 		default :
-			HAL_GPIO_WritePin(GPIOC, motorA_dir, 0);
-			pwnA_ccr=0;
+			pwnA_ccrA = 0;
+			pwnA_ccrB = 0;
 			pwnA_cnt=0;
 	}
 }
 void motorB(char c,int speed){
 	switch(c){
 		case 'f':
-			HAL_GPIO_WritePin(GPIOA, motorB_dir, 1);
-			pwnB_ccr=speed;
+			pwnB_ccrA = speed;
+			pwnB_ccrB = 0;
 			pwnB_cnt=0;
 			break;
 		case 'b':
-			HAL_GPIO_WritePin(GPIOA, motorB_dir, 0);
-			pwnB_ccr=speed;
+			pwnB_ccrA = 0;
+			pwnB_ccrB = speed;
 			pwnB_cnt=0;
 			break;
 		default :
-			HAL_GPIO_WritePin(GPIOA, motorB_dir, 0);
-			pwnB_ccr=0;
+			pwnB_ccrA = 0;
+			pwnB_ccrB = 0;
 			pwnB_cnt=0;
 	}
 }
-
+void forward(){
+	motorA('f',100);
+	motorB('f',100);
+}
+void backward(){
+	motorA('b',100);
+	motorB('b',100);
+}
+void left(){
+	motorA('f',50);
+	motorB('b',50);
+}
+void right(){
+	motorA('b',50);
+	motorB('f',50);
+}
+void stop(){
+	motorA('o',0);
+	motorB('o',0);
+}
 #endif
