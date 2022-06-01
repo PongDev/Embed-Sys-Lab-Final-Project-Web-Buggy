edebd41734ae065afd3cf570b384eea6326e61f1
MinitrueSecretary <wipugeng@hotmail.com>
2022-06-01 20:24:43 +0700

    add first text change

--- Server/static/lang.js
+++ Server/static/lang.js
@@ -1,11 +1,16 @@
 var thaiButton = document.getElementById("thaibutton")
 var engButton = document.getElementById("engbutton")
+
+var tableheads = document.getElementsByTagName("th")
 function toThai(){
     thaiButton.className = "langbuttonon"
     engButton.className = "langbuttonoff"
 
     thaiButton.disabled = true;
     engButton.disabled = false;
+
+    tableheads[0].innerHTML = "ระยะห่างจากสิ่งกีดขวางด้านหน้า (ซม)"
+    tableheads[1].innerHTML = "สถานะของไฟหน้ารถ"
 }
 
 function toEnglish(){
@@ -14,4 +19,7 @@ function toEnglish(){
 
     thaiButton.disabled = false;
     engButton.disabled = true;
+
+    tableheads[0].innerHTML = "Distance from Front Obstacle (cm)"
+    tableheads[1].innerHTML = "Headlight Status"
 }
